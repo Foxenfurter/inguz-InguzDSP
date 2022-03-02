9193387ed04047a15e758548789a922946d11227
Foxenfurter <94196884+Foxenfurter@users.noreply.github.com>
2022-03-02 22:21:48 +0000

    Build Refreshes
    Includes 32 bit Arm built which is the version required for Pi

--- PublishInguz.bat
+++ PublishInguz.bat
@@ -6,6 +6,9 @@ dotnet publish -c Release -r win-x86 -o publishWin32   --self-contained true  -p
 REM Publish for linux-x64 
 REM dotnet publish -c Release -r linux-arm64 -o publishlinux-arm64  -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
 dotnet publish -c Release -r linux-arm64 -o publishlinux-arm64   --self-contained true  -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=True
+
+dotnet publish -c Release -r linux-arm -o publishlinux-arm   --self-contained true  -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=True
+
 REM Publish for osx.11.0-x64
 REM dotnet publish -c Release -r osx-x64 -o publishOsx-x64 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
 dotnet publish -c Release -r osx-x64 -o publishOsx-x64 --self-contained true  -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=True
